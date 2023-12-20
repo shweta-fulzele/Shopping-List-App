4f8488145a9f692e74a25aff9ce33ab7afc7d5e8
Shweta-Fulzele <shweta.fulzele@thinkitive.com>
2023-12-20 16:14:15 +0530

    enhanced the UI along with functionality

--- app/src/main/java/com/base/shoppinglist/userinterfaceutils/UiComponent.kt
+++ app/src/main/java/com/base/shoppinglist/userinterfaceutils/UiComponent.kt
@@ -5,8 +5,11 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.material3.Button
@@ -15,6 +18,10 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
@@ -50,6 +57,23 @@ fun HeightSpacer(height: Int) {
             .height(height.dp)
             .fillMaxWidth()
     )
+}
+
+@Composable
+fun CustomBasicTextField(editedName : String ){
+
+    var edited by remember {
+        mutableStateOf(editedName)
+    }
+    BasicTextField(
+        value = editedName,
+        onValueChange = { newText ->  edited = newText },
+        singleLine = true,
+        modifier = Modifier
+            .wrapContentSize()
+            .padding(8.dp)
+    )
+
 }
 
 
